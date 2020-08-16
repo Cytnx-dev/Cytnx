5b962f0df5c8d3cbc055226ec0f2b9d1c96968dd
kaihsinwu <kaihsinwu@gmail.com>
2020-08-16 04:37:47 -0400

    update, add compile flag in python attrib for handy compiling and linking

--- cytnx/__init__.py
+++ cytnx/__init__.py
@@ -12,8 +12,8 @@ if(os.path.exists(os.path.join(os.path.dirname(__file__),"include"))):
     __cpp_include__=os.path.join(os.path.dirname(__file__),"include")
     __cpp_lib__=os.path.join(os.path.dirname(__file__),"lib")
 else:
-    __cpp_include__=''
-    __cpp_lib__=''
+    __cpp_include__=os.path.join(os.path.dirname(os.path.dirname(__file__)),"include")
+    __cpp_lib__=os.path.join(os.path.dirname(os.path.dirname(__file__)),"lib")
 
 __blasINTsize__ = cytnx.__blasINTsize__
 
@@ -22,4 +22,19 @@ def from_numpy(np_arr):
     if np_arr.flags['C_CONTIGUOUS'] == False:
         tmp = numpy.ascontiguousarray(np_arr)
     return cytnx._from_numpy(tmp)
+
+def _resolve_cpp_linkflags__():
+    f = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"linkflags.tmp"))
+    lines = f.readlines()
+    out = ""
+    for line in lines:
+        line = line.strip()
+        out+=line.replace(";"," ")
+        out+=" "
+    f.close()
+    return out
+
+
+__cpp_linkflags__ = _resolve_cpp_linkflags__()
+
 
