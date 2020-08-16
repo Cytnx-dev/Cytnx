2f1e5c30addd2b5496f29b560faa5e6d6e5da684
kaihsinwu <kaihsinwu@gmail.com>
2020-08-16 08:20:05 -0400

    add compile.sh

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ export CYTNX_INC=$(python -c "exec(\"import cytnx\nprint(cytnx.__cpp_include__)\
 export CYTNX_LIB=$(python -c "exec(\"import cytnx\nprint(cytnx.__cpp_lib__)\")")
 export CYTNX_LINK="$(python -c "exec(\"import cytnx\nprint(cytnx.__cpp_linkflags__)\")")"
 
-g++ -std=c++11 -fopenmp -I${CYTNX_INC} test.cpp ${CYTNX_LIB}/libcytnx.a ${CYTNX_LINK} -o test
+g++ -std=c++11 -I${CYTNX_INC} test.cpp ${CYTNX_LIB}/libcytnx.a ${CYTNX_LINK} -o test
 
