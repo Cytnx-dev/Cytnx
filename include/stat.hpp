3659dcf7f47c322c8bd3893fd28d4410a4c4c828
kaihsin <kaihsinwu@gmail.com>
2022-03-14 16:22:04 -0400

    add Histogram2d

--- include/stat.hpp
+++ include/stat.hpp
@@ -7,6 +7,10 @@
 #include "Tensor.hpp"
 #include <algorithm>
 #include <iostream>
+
+
+
+
 namespace cytnx{
     namespace stat{
         
@@ -94,9 +98,113 @@ namespace cytnx{
                     return this->x.size();
                 }
 
+               
         };//class histogram 
 
+        /// 2D, real value histogram 
+        class Histogram2d{
+            public:
+                double minx,miny;
+                double maxx,maxy;
+                uint64_t binx,biny;
+                cytnx::Storage vars;
+                cytnx::Storage x;
+                cytnx::Storage y;
 
+                //std::vector<double> vars;
+                //std::vector<double> x;
+
+                double total_count;
+                
+               
+                /**
+                @brief initialize a histogram 
+                */            
+                Histogram2d(const unsigned long long &Nbinx, const unsigned long long &Nbiny, const double &min_x, const double &max_x, const double &min_y, const double &max_y);
+               
+                ///@cond
+                Histogram2d(const Histogram2d &rhs){
+                    this->minx = rhs.minx;
+                    this->maxx = rhs.maxx;
+                    this->miny = rhs.miny;
+                    this->maxy = rhs.maxy;
+                    this->binx = rhs.binx;
+                    this->biny = rhs.biny;
+                    this->vars = rhs.vars.clone();
+                    this->x    = rhs.x.clone();
+                    this->y    = rhs.y.clone();
+                    this->total_count = rhs.total_count;
+                }
+
+                Histogram2d& operator=(const Histogram2d &rhs){
+                    this->minx = rhs.minx;
+                    this->maxx = rhs.maxx;
+                    this->miny = rhs.miny;
+                    this->maxy = rhs.maxy;
+                    this->binx = rhs.binx;
+                    this->biny = rhs.biny;
+                    this->vars = rhs.vars.clone();
+                    this->x    = rhs.x.clone();
+                    this->y    = rhs.y.clone();
+                    this->total_count = rhs.total_count;    
+                    return *this;
+                }
+                ///@endcond
+     
+                void clear_vars(){
+                    total_count = 0;
+                    memset(this->vars.data(),0,sizeof(double)*this->vars.size());
+                }        
+
+                template<class T>
+                void accumulate(const std::vector<T> &data_x,const std::vector<T> &data_y){
+                    //[not fin!]
+                    cytnx_error_msg(data_x.size()!=data_y.size(), "[ERROR][Histogram2d::accumulate] data_x and data_y should have same size!%s","\n");
+
+                    double dx = double(maxx-minx)/binx;
+                    double dy = double(maxy-miny)/biny;
+
+                    unsigned int nx,ny;
+
+                    // for each elem in data, compare
+                    for(unsigned long long i=0;i<data_x.size();i++){
+                       
+                        nx = floor(data_x[i]/dx);
+                        ny = floor(data_y[i]/dy);  
+                        vars.at<double>(ny*binx+nx) +=1; total_count+=1; 
+                       
+                    }    
+                }
+
+                void normalize();
+                void print() const;                
+                
+                const Storage& get_x()const{
+                    //get x 
+                    return this->x;
+                }
+                Storage& get_x(){
+                    //get x 
+                    return this->x;
+                }
+
+                const Storage& get_y()const{
+                    //get y
+                    return this->y;
+                }
+                Storage& get_y(){
+                    //get y
+                    return this->y;
+                }
+                
+                std::vector<cytnx_uint64> size(){
+                    return std::vector<cytnx_uint64>({this->x.size(),this->y.size()});
+                }
+                
+
+
+               
+        };//class histogram 
         
 
         //function, statistical:
--- src/stat/histogram.cpp
+++ src/stat/histogram.cpp
@@ -44,6 +44,60 @@ namespace cytnx{
             std::cout << "current count: " << this->total_count << std::endl;
         }
 
+        //-----------[2d]
+        Histogram2d::Histogram2d(const unsigned long long &Nbinx, const unsigned long long &Nbiny, const double &min_x, const double &max_x, const double &min_y, const double &max_y){
+            if(min_x >= max_x){
+                std::cout << "[ERROR] cannot have min >= max [x axis]" << std::endl;exit(1);
+            }
+            if(min_y >= max_y){
+                std::cout << "[ERROR] cannot have min >= max [y axis]" << std::endl;exit(1);
+            }
+            this->minx = min_x;
+            this->maxx = max_x;
+            this->miny = min_y;
+            this->maxy = max_y;
+
+            this->binx = Nbinx;
+            this->biny = Nbiny;
+
+            this->vars = Storage(Nbinx*Nbiny,Type.Double);
+            this->x = Storage(Nbinx,Type.Double);
+            this->y = Storage(Nbiny,Type.Double);
+
+            double dx = double(max_x-min_x)/Nbinx;
+            for(unsigned int i=0;i<x.size();i++){
+                this->x.at<double>(i) = dx*i;
+            }
+            double dy = double(max_y-min_y)/Nbiny;
+            for(unsigned int i=0;i<y.size();i++){
+                this->y.at<double>(i) = dy*i;
+            }
+            total_count = 0;
+
+        }
+
+        void Histogram2d::normalize(){
+            //get the density. 
+            double dx = double(maxx-minx)/binx;
+            double dy = double(maxy-miny)/biny;
+            double w = 1./(total_count*dx*dy);
+
+            //wrapping around storage and use Tensor API
+            Tensor tmp = Tensor::from_storage(this->vars);
+            tmp *= w;
+            this->vars = tmp.storage();
+
+
+        }
+
+        void Histogram2d::print() const{
+            std::cout << "[Histogram 2D] Real" << std::endl;
+            std::cout << "Nbins: [x= " << this->binx << " , y= " << this->biny << " ]\n";
+            std::cout << "bound,x: [ " << this->minx << " , " << this->maxx << " ]\n";
+            std::cout << "bound,y: [ " << this->miny << " , " << this->maxy << " ]\n";
+            std::cout << "current count: " << this->total_count << std::endl;
+        }
+
 
     }//stat
 }//cytnx
