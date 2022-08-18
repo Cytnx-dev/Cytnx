796fb2218b6ba2207c3a4562f51b67b6ca15e3f1
kaihsin <kaihsinwu@gmail.com>
2022-08-17 20:08:04 -0400

    update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -10,7 +10,7 @@ var searchData=
   ['binx',['binx',['../classcytnx_1_1stat_1_1Histogram2d.html#a0fbad9cdd602b54204b5e8f906b496cb',1,'cytnx::stat::Histogram2d']]],
   ['biny',['biny',['../classcytnx_1_1stat_1_1Histogram2d.html#a452378fb0518636365ff047260156674',1,'cytnx::stat::Histogram2d']]],
   ['blas_5fint',['blas_int',['../lapack__wrapper_8hpp.html#a811069b79e704a9e1dee1e70bb135aec',1,'lapack_wrapper.hpp']]],
-  ['bond',['Bond',['../classcytnx_1_1Bond.html',1,'cytnx::Bond'],['../classcytnx_1_1Bond.html#a80ee6b9a0dd288e74d885f6f288b5a0c',1,'cytnx::Bond::Bond()']]],
+  ['bond',['Bond',['../classcytnx_1_1Bond.html',1,'cytnx::Bond'],['../classcytnx_1_1Bond.html#a07761956ff816dfb022413a1e0085d77',1,'cytnx::Bond::Bond()']]],
   ['bond_2ecpp',['Bond.cpp',['../Bond_8cpp.html',1,'']]],
   ['bond_2ehpp',['Bond.hpp',['../Bond_8hpp.html',1,'']]],
   ['bonds',['bonds',['../classcytnx_1_1UniTensor.html#abc8348e0f56e2479dbe45273c3055b1b',1,'cytnx::UniTensor::bonds() const'],['../classcytnx_1_1UniTensor.html#aa5d60a640bf9e17f05a18fa3016c46d0',1,'cytnx::UniTensor::bonds()']]],
