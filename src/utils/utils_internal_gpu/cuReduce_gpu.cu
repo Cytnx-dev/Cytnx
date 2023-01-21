256f327b4a91b7a209e32741fbd0bbeec6c8532e
kaihsin <kaihsinwu@gmail.com>
2023-01-21 05:19:47 -0500

    refactor cuReduce and add i32 u32 i64 u64 versions

--- src/utils/utils_internal_gpu/cuReduce_gpu.cu
+++ src/utils/utils_internal_gpu/cuReduce_gpu.cu
@@ -8,15 +8,29 @@ namespace cytnx{
 
 
         template <class X> 
-        __device__ void warp_unroll(volatile X *smem,int thidx){
+        __device__ void warp_unroll(X *smem,int tid){
+          X v = 0;
+          v += smem[tid+32]; __syncwarp();
+          smem[tid] = v;
+          v += smem[tid+16]; __syncwarp();
+          smem[tid] = v;
+          v += smem[tid+8]; __syncwarp();
+          smem[tid] = v;
+          v += smem[tid+4]; __syncwarp();
+          smem[tid] = v;
+          v += smem[tid+2]; __syncwarp();
+          smem[tid] = v;
+          v += smem[tid+1]; __syncwarp();
+          smem[tid] = v;
 
+          /* deprecated after volta, warp_unroll(volatile X*smem, int thidx);
           smem[thidx]+=smem[thidx + 32];
           smem[thidx]+=smem[thidx + 16];
           smem[thidx]+=smem[thidx + 8 ];
           smem[thidx]+=smem[thidx + 4 ];
           smem[thidx]+=smem[thidx + 2 ];
           smem[thidx]+=smem[thidx + 1 ];
-
+          */
         }
 
 
@@ -47,14 +61,31 @@ namespace cytnx{
         //=======================
 
         
-        __device__ void warp_unroll(volatile cuDoubleComplex *smem,int thidx){
+        __device__ void warp_unroll(cuDoubleComplex *smem,int tid){
 
+          cuDoubleComplex v = make_cuDoubleComplex(0,0);
+          v.x += smem[tid+32].x; v.y += smem[tid+32].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+16].x; v.y += smem[tid+16].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+8].x; v.y += smem[tid+8].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+4].x; v.y += smem[tid+4].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+2].x; v.y += smem[tid+2].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+1].x; v.y += smem[tid+1].y;  __syncwarp();
+          smem[tid] = v;
+
+
+          /* deprecated after volta, warp_unroll(volatile X*smem, int thidx);
           smem[thidx].x += smem[thidx + 32].x; smem[thidx].y += smem[thidx + 32].y;
           smem[thidx].x += smem[thidx + 16].x; smem[thidx].y += smem[thidx + 16].y;
           smem[thidx].x += smem[thidx + 8].x; smem[thidx].y += smem[thidx + 8].y;
           smem[thidx].x += smem[thidx + 4].x; smem[thidx].y += smem[thidx + 4].y;
           smem[thidx].x += smem[thidx + 2].x; smem[thidx].y += smem[thidx + 2].y;
           smem[thidx].x += smem[thidx + 1].x; smem[thidx].y += smem[thidx + 1].y;
+          */
 
         }
         __global__ void cuReduce_kernel_cd(cuDoubleComplex* out, cuDoubleComplex* in, cytnx_uint64 Nelem){
@@ -92,14 +123,30 @@ namespace cytnx{
 
         }
         
-        __device__ void warp_unroll(volatile cuFloatComplex *smem,int thidx){
+        __device__ void warp_unroll(cuFloatComplex *smem,int tid){
+          cuFloatComplex v = make_cuFloatComplex(0,0);
+          v.x += smem[tid+32].x; v.y += smem[tid+32].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+16].x; v.y += smem[tid+16].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+8].x; v.y += smem[tid+8].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+4].x; v.y += smem[tid+4].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+2].x; v.y += smem[tid+2].y;  __syncwarp();
+          smem[tid] = v;
+          v.x += smem[tid+1].x; v.y += smem[tid+1].y;  __syncwarp();
+          smem[tid] = v;
 
+          /* deprecated after volta.
           smem[thidx].x += smem[thidx + 32].x; smem[thidx].y += smem[thidx + 32].y;
           smem[thidx].x += smem[thidx + 16].x; smem[thidx].y += smem[thidx + 16].y;
           smem[thidx].x += smem[thidx + 8].x; smem[thidx].y += smem[thidx + 8].y;
           smem[thidx].x += smem[thidx + 4].x; smem[thidx].y += smem[thidx + 4].y;
           smem[thidx].x += smem[thidx + 2].x; smem[thidx].y += smem[thidx + 2].y;
           smem[thidx].x += smem[thidx + 1].x; smem[thidx].y += smem[thidx + 1].y;
+          */
+
         }
         __global__ void cuReduce_kernel_cf(cuFloatComplex* out, cuFloatComplex* in, cytnx_uint64 Nelem){
             __shared__ cuFloatComplex sD[_TNinB_REDUCE_]; // allocate share mem for each thread
@@ -143,10 +190,9 @@ namespace cytnx{
             a = b;
             b = tmp; 
         }
-
-        void cuReduce_gpu_d(double* out, double* in, const cytnx_uint64 &Nelem){
-            //cytnx_double * outptr = (cytnx_double*)out->Mem;
-            //cytnx_double * ptr = (cytnx_double*)in->Mem;
+    
+        template<class T>
+        void cuReduce_gpu_generic(T* out, T*in, const cytnx_uint64 &Nelem){
             cytnx_uint64 Nelems = Nelem;
             cytnx_uint64 NBlocks;
 
@@ -154,9 +200,9 @@ namespace cytnx{
             if(Nelems%_TNinB_REDUCE_) NBlocks+=1;
 
             //alloc mem for each block:
-            cytnx_double *dblk;
+            T *dblk;
             //std::cout << NBlocks*sizeof(cytnx_double) << std::endl;
-            cudaMalloc((void**)&dblk,NBlocks*sizeof(cytnx_double));
+            cudaMalloc((void**)&dblk,NBlocks*sizeof(T));
             
               
             if(NBlocks==1){
@@ -173,8 +219,8 @@ namespace cytnx{
                 if(NBlocks==1){
                     cuReduce_kernel<<<NBlocks,_TNinB_REDUCE_>>>(out,dblk,Nelems);
                 }else{
-                    cytnx_double *dblk2;
-                    cudaMalloc((void**)&dblk2,NBlocks*sizeof(cytnx_double));
+                    T *dblk2;
+                    cudaMalloc((void**)&dblk2,NBlocks*sizeof(T));
                     // do something:
                     cuReduce_kernel<<<NBlocks,_TNinB_REDUCE_>>>(dblk2,dblk,Nelems);
 
@@ -184,51 +230,30 @@ namespace cytnx{
                 Nelems = NBlocks;
             }
             cudaFree(dblk);
+        }
 
-            
+        void cuReduce_gpu_d(double* out, double* in, const cytnx_uint64 &Nelem){
+            cuReduce_gpu_generic(out,in,Nelem);
         }
 
         void cuReduce_gpu_f(float* out, float* in, const cytnx_uint64 &Nelem){
-            cytnx_uint64 Nelems = Nelem;
-            cytnx_uint64 NBlocks;
-
-            NBlocks = Nelems/_TNinB_REDUCE_;
-            if(Nelems%_TNinB_REDUCE_) NBlocks+=1;
-
-            //alloc mem for each block:
-            cytnx_float *dblk;
-            //std::cout << NBlocks*sizeof(cytnx_double) << std::endl;
-            cudaMalloc((void**)&dblk,NBlocks*sizeof(cytnx_float));
-            
-              
-            if(NBlocks==1){
-                cuReduce_kernel<<<NBlocks,_TNinB_REDUCE_>>>(out,in,Nelems);
-            }else{
-                cuReduce_kernel<<<NBlocks,_TNinB_REDUCE_>>>(dblk,in,Nelems);
-            }
-            Nelems = NBlocks;
-
-            while(Nelems>1){
-                NBlocks = Nelems/_TNinB_REDUCE_;
-                if(Nelems%_TNinB_REDUCE_) NBlocks+=1;
-
-                if(NBlocks==1){
-                    cuReduce_kernel<<<NBlocks,_TNinB_REDUCE_>>>(out,dblk,Nelems);
-                }else{
-                    cytnx_float *dblk2;
-                    cudaMalloc((void**)&dblk2,NBlocks*sizeof(cytnx_float));
-                    // do something:
-                    cuReduce_kernel<<<NBlocks,_TNinB_REDUCE_>>>(dblk2,dblk,Nelems);
-
-                    swap(dblk2,dblk); //swap new data to old data, and free the old
-                    cudaFree(dblk2);
-                }
-                Nelems = NBlocks;
-            }
-            cudaFree(dblk);
-
-            
+            cuReduce_gpu_generic(out,in,Nelem);
         }
+
+        void cuReduce_gpu_i64(cytnx_int64* out, cytnx_int64* in, const cytnx_uint64 &Nelem){
+            cuReduce_gpu_generic(out,in,Nelem);
+        }
+        void cuReduce_gpu_u64(cytnx_uint64* out, cytnx_uint64* in, const cytnx_uint64 &Nelem){
+            cuReduce_gpu_generic(out,in,Nelem);
+        }
+
+        void cuReduce_gpu_i32(cytnx_int32* out, cytnx_int32* in, const cytnx_uint64 &Nelem){
+            cuReduce_gpu_generic(out,in,Nelem);
+        }
+        void cuReduce_gpu_u32(cytnx_uint32* out, cytnx_uint32* in, const cytnx_uint64 &Nelem){
+            cuReduce_gpu_generic(out,in,Nelem);
+        }
+
 
         void cuReduce_gpu_cf(cytnx_complex64* out, cytnx_complex64* in, const cytnx_uint64 &Nelem){
             cytnx_uint64 Nelems = Nelem;
