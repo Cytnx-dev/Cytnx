b57a990b5c279ae202e98385ae7b393e2e265d64
j9263178 <j9263178@gmail.com>
2023-02-20 01:06:42 +0800

    fix svd test data cannot be loaded

--- tests/linalg_test/linalg_bk_test.h
+++ tests/linalg_test/linalg_bk_test.h
@@ -1,13 +1,16 @@
-#ifndef _H_linalg_test
-#define _H_linalg_test
+#ifndef _H_linalg_bk_test
+#define _H_linalg_bk_test
 
 #include "cytnx.hpp"
 #include <gtest/gtest.h>
 
 using namespace cytnx;
 
+std::string data_dir = "../../tests/test_data_base/linalg/";
+
 class linalg_bk_Test : public ::testing::Test {
  public:
+  
   // ==================== svd_truncate ===================
   Bond svd_I = Bond(BD_OUT,{Qs(1),Qs(-1)},{1,1});
   Bond svd_J = Bond(BD_IN,{Qs(5),Qs(3),Qs(1),Qs(-1),Qs(-3),Qs(-5),Qs(-7)},{6,22,57,68,38,8,1});
@@ -16,19 +19,23 @@ class linalg_bk_Test : public ::testing::Test {
   UniTensor svd_T = UniTensor({svd_I,svd_J,svd_K,svd_L},{"a","b","c","d"},1,Type.Double,Device.cpu,false);
   Tensor svd_Sans;
   //==================== Lanczos_Gnd_Ut ===================
-  Tensor A = Tensor::Load("../test_data_base/linalg/Lanczos_Gnd/lan_block_A.cytn");
-  Tensor B = Tensor::Load("../test_data_base/linalg/Lanczos_Gnd/lan_block_B.cytn");
-  Tensor C = Tensor::Load("../test_data_base/linalg/Lanczos_Gnd/lan_block_C.cytn");
+  Tensor A = Tensor::Load(data_dir+"Lanczos_Gnd/lan_block_A.cytn");
+  Tensor B = Tensor::Load(data_dir+"Lanczos_Gnd/lan_block_B.cytn");
+  Tensor C = Tensor::Load(data_dir+"Lanczos_Gnd/lan_block_C.cytn");
   Bond lan_I = Bond(BD_IN,{Qs(-1),Qs(0),Qs(1)},{9,9,9});
   Bond lan_J = Bond(BD_OUT,{Qs(-1),Qs(0),Qs(1)},{9,9,9});
   UniTensor H = UniTensor({lan_I, lan_J});
 
+  //==================== QR ===================
+  Tensor Qr_Qans = Tensor::Load(data_dir+"Qr/qr_Qans.cytn");
+  Tensor Qr_Rans = Tensor::Load(data_dir+"Qr/qr_Rans.cytn");
+
  protected:
   void SetUp() override {
     //================ svd truncate =======================
-    svd_T = svd_T.Load("../test_data_base/linalg/Svd_truncate/Svd_truncate1.cytnx");
+    svd_T = svd_T.Load(data_dir+"Svd_truncate/Svd_truncate1.cytnx");
     svd_T.set_rowrank(2);
-    svd_Sans = Tensor::Load("../test_data_base/linalg/Svd_truncate/S_truncate1.cytn"); 
+    svd_Sans = Tensor::Load(data_dir+"Svd_truncate/S_truncate1.cytn"); 
     svd_Sans = algo::Sort(svd_Sans);
     //==================== Lanczos_Gnd_Ut ===================
     H.put_block(A,0);
