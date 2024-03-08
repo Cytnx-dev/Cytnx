020eb900b4b5a82edd780f471c66db917c565687
j9263178 <j9263178@gmail.com>
2024-03-08 19:08:09 +0800

    test

--- pytests/tests.py
+++ pytests/tests.py
@@ -1,7 +1,7 @@
+import sys, os
+sys.path.append('/home/runner/work/Cytnx_lib')
+
 from cytnx import *
-
-# pytest test
-
 T = zeros([4,4])
 CyT = UniTensor(T,rowrank=2) #create un-tagged UniTensor from Tensor
 CyT.print_diagram()
