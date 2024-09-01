72453ca044a5477ddf29a65bb5ea289eccf636c0
Ivana <ivana.gyro@gmail.com>
2024-09-01 17:05:07 +0800

    Suppress Clang compilation errors on Mac
    An issue reported the code below raises error by clang on Mac.
    
    ```
    doulbe _Complex right = 123.4 + 45435.1I;
    std::complex<doulbe> left{43.4, 2532.1};
    left *= (std::complex<doulbe>)right; // invalid implicit conversion
    ```
    
    This may be a bug in Clang on Mac, as no errors are raised by Clang on
    CentOS or in the online compiler.

--- src/backend/linalg_internal_cpu/Det_internal.cpp
+++ src/backend/linalg_internal_cpu/Det_internal.cpp
@@ -1,7 +1,12 @@
 #include "Det_internal.hpp"
-#include "../utils_internal_interface.hpp"
-#include "utils/utils.hpp"
+
+#include <complex.h>
+#include <type_traits>
+
 #include "backend/lapack_wrapper.hpp"
+#include "backend/utils_internal_interface.hpp"
+#include "utils/utils.hpp"
+
 #ifdef UNI_OMP
   #include <omp.h>
 #endif
@@ -25,10 +30,22 @@ namespace cytnx {
       cytnx_error_msg(
         info != 0, "%s %d",
         "[ERROR][Det_internal] Error in Lapack function 'zgetrf': Lapack INFO = ", info);
+
+      // Whether lapack_complex_T is defined as std::complex<T> (c++ complex) or T _Complex
+      // (C complex) depends on whether MKL is used.
+      auto lapack_complex_to_cytnx_complex = [](auto x) {
+        if constexpr (std::is_same_v<std::complex<double>, decltype(x)>) {
+          return static_cast<cytnx_complex128>(x);
+        } else if constexpr (std::is_same_v<double _Complex, decltype(x)>) {
+          return cytnx_complex128{creal(x), cimag(x)};
+        } else {
+          assert(false);
+        }
+      };
       od[0] = 1;
       bool neg = 0;
       for (lapack_int i = 0; i < N; i++) {
-        od[0] *= (cytnx_complex128)_Rin[i * N + i];
+        od[0] *= lapack_complex_to_cytnx_complex(_Rin[i * N + i]);
         if (ipiv[i] != (i + 1)) neg = !neg;
       }
       free(ipiv);
@@ -49,10 +66,22 @@ namespace cytnx {
       cytnx_error_msg(
         info != 0, "%s %d",
         "[ERROR][Det_internal] Error in Lapack function 'cgetrf': Lapack INFO = ", info);
+
+      // Whether lapack_complex_T is defined as std::complex<T> (c++ complex) or T _Complex
+      // (C complex) depends on whether MKL is used.
+      auto lapack_complex_to_cytnx_complex = [](auto x) {
+        if constexpr (std::is_same_v<std::complex<float>, decltype(x)>) {
+          return static_cast<cytnx_complex64>(x);
+        } else if constexpr (std::is_same_v<float _Complex, decltype(x)>) {
+          return cytnx_complex64{crealf(x), cimagf(x)};
+        } else {
+          assert(false);
+        }
+      };
       od[0] = 1;
       bool neg = 0;
       for (lapack_int i = 0; i < N; i++) {
-        od[0] *= (cytnx_complex64)_Rin[i * N + i];
+        od[0] *= lapack_complex_to_cytnx_complex(_Rin[i * N + i]);
         if (ipiv[i] != (i + 1)) neg = !neg;
       }
       free(ipiv);
