4b45653c8c2c81f166b4cb4f92e04aa41896b516
Manuel Schneider <manuel.schneider@nycu.edu.tw>
2024-09-10 16:23:28 +0200

    fixed segfault when a BlockUniTensor with no valid blocks was printed/displayed

--- include/UniTensor.hpp
+++ include/UniTensor.hpp
@@ -1171,28 +1171,30 @@ namespace cytnx {
       cytnx_error_msg(this->_blocks.size() == 0, "[ERROR][internal] empty blocks for blockform.%s",
                       "\n");
   #endif
-      return this->_blocks[0].dtype();
+      return this->_blocks.size() < 1 ? N_Type + 1 : this->_blocks[0].dtype();
     };
     int device() const {
   #ifdef UNI_DEBUG
       cytnx_error_msg(this->_blocks.size() == 0, "[ERROR][internal] empty blocks for blockform.%s",
                       "\n");
   #endif
-      return this->_blocks[0].device();
+      return this->_blocks.size() < 1 ? 404 : this->_blocks[0].device();
     };
     std::string dtype_str() const {
   #ifdef UNI_DEBUG
       cytnx_error_msg(this->_blocks.size() == 0, "[ERROR][internal] empty blocks for blockform.%s",
                       "\n");
   #endif
-      return this->_blocks[0].dtype_str();
+      return this->_blocks.size() < 1 ? "No valid blocks, so no dtype!"
+                                      : this->_blocks[0].dtype_str();
     };
     std::string device_str() const {
   #ifdef UNI_DEBUG
       cytnx_error_msg(this->_blocks.size() == 0, "[ERROR][internal] empty blocks for blockform.%s",
                       "\n");
   #endif
-      return this->_blocks[0].device_str();
+      return this->_blocks.size() < 1 ? "No valid blocks, so no device!"
+                                      : this->_blocks[0].device_str();
     };
 
     Tensor get_block(const cytnx_uint64 &idx = 0) const {
