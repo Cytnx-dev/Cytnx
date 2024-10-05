1acce4c42d5ccef57bdf714a3f42d87413ace28e
Ivana <ivana.gyro@gmail.com>
2024-10-06 00:36:31 +0800

    Add an assertion for checking equality of numbers
    This assertion consumes all supported data types. It's useful in
    type-parameterized tess. The difference between EXPECT_NUMBER_EQ and
    gtest's EXPECT_EQ is that EXPECT_NUMBER_EQ compares the floating numbers
    with their ULP distance.

--- tests/gpu/test_tools.h
+++ tests/gpu/test_tools.h
@@ -61,7 +61,64 @@ namespace cytnx {
                      const std::vector<cytnx_uint64>& idices2);
 
     void InitUniTensorUniform(UniTensor& UT, unsigned int rand_seed = 0);
+
+    /**
+     * Helper function for the EXPECT_NUMBER_EQ assertion.
+     * This function compares floating-point numbers, considering their ULP (Units in Last Place)
+     * distance. If the distance between two floating-point numbers is less than 4 ULPs, they are
+     * considered equal.
+     */
+    template <typename T1, typename T2>
+    ::testing::AssertionResult AreEqNumber(const char* expr1, const char* expr2, T1 value1,
+                                           T2 value2) {
+      static_assert(std::is_arithmetic_v<T1> && std::is_arithmetic_v<T2>);
+      // Prevent unintentional comparison between float and double.
+      static_assert(std::is_floating_point_v<T1> && std::is_floating_point_v<T2>
+                      ? std::is_same_v<T1, T2>
+                      : true);
+      if constexpr (std::is_integral_v<T1> && std::is_integral_v<T2>) {
+        return ::testing::internal::EqHelper::Compare(expr1, expr2, value1, value2);
+      }
+      if constexpr (std::is_floating_point_v<T1>) {
+        return ::testing::internal::CmpHelperFloatingPointEQ<T1>(expr1, expr2, value1, value2);
+      }
+      // std::is_floating_point_v<T2>
+      return ::testing::internal::CmpHelperFloatingPointEQ<T2>(expr1, expr2, value1, value2);
+    }
+
+    /**
+     * Complex floating-point numbers are compared based on the ULP distances of their real and
+     * imaginary parts, rather than using the Euclidean distance between the two complex numbers.
+     */
+    template <typename T>
+    ::testing::AssertionResult AreEqNumber(const char* expr1, const char* expr2,
+                                           const std::complex<T>& value1,
+                                           const std::complex<T>& value2) {
+      ::testing::internal::FloatingPoint<T> float1_real(value1.real()), float1_imag(value1.imag()),
+        float2_real(value2.real()), float2_imag(value2.imag());
+      if (float1_real.AlmostEquals(float2_real) && float1_imag.AlmostEquals(float2_imag)) {
+        return ::testing::AssertionSuccess();
+      }
+
+      std::stringstream value1_stream;
+      value1_stream.precision(std::numeric_limits<T>::digits10 + 2);
+      value1_stream << value1.real() << "+" << value1.imag() << "j";
+
+      std::stringstream value2_stream;
+      value2_stream.precision(std::numeric_limits<T>::digits10 + 2);
+      value2_stream << value2.real() << "+" << value2.imag() << "j";
+      return ::testing::internal::EqFailure(expr1, expr2, value1_stream.str(), value2_stream.str(),
+                                            false);
+    }
   }  // namespace TestTools
 }  // namespace cytnx
+
+/**
+ * This assertion handles all supported data types and is useful in type-parameterized tests.
+ * The key difference between EXPECT_NUMBER_EQ and gtest's EXPECT_EQ is that EXPECT_NUMBER_EQ
+ * compares floating-point numbers based on their ULP (Units in Last Place) distance.
+ */
+#define EXPECT_NUMBER_EQ(value1, value2) \
+  EXPECT_PRED_FORMAT2(cytnx::TestTools::AreEqNumber, value1, value2)
 
 #endif  // GPU_TEST_TOOLS_H_
