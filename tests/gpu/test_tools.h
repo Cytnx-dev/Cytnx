0a464ef3f688d5c29190d8a63997f31b0470347e
Ivana <ivana.gyro@gmail.com>
2024-10-05 20:39:26 +0800

    Move namespace TestTools under namespace cytnx
    Avoid using using-directives in the header files.

--- tests/gpu/test_tools.h
+++ tests/gpu/test_tools.h
@@ -1,60 +1,67 @@
-#ifndef _H_test_tools
-#define _H_test_tools
+#ifndef GPU_TEST_TOOLS_H_
+#define GPU_TEST_TOOLS_H_
 
-#include "cytnx.hpp"
-#include <gtest/gtest.h>
+#include <string>
+#include <vector>
 
-// this file contains some function we may usually use in the unit test
-//   such as the data initialization and comparison.
+#include "gtest/gtest.h"
 
-using namespace cytnx;
+#include "Device.hpp"
+#include "Tensor.hpp"
+#include "Type.hpp"
+#include "UniTensor.hpp"
 
-namespace TestTools {
+// This file contains some function we may usually use in the unit test
+// such as the data initialization and comparison.
+namespace cytnx {
+  namespace TestTools {
 
-  // test error message trace
-  class TestFailMsg {
-   private:
-    std::string test_case;
-    std::vector<std::string> fail_msgs;
+    // test error message trace
+    class TestFailMsg {
+     private:
+      std::string test_case;
+      std::vector<std::string> fail_msgs;
 
-   public:
-    // TestFailMsg();
-    void Init(const std::string& _test_case) {
-      test_case = _test_case;
-      fail_msgs.clear();
-    }
-    void AppendMsg(const std::string& fail_msg, const std::string& func_name, const int line);
-    void AppendMsg(const std::string& fail_msg) { fail_msgs.push_back(fail_msg); }
-    bool is_empty() { return !(test_case.empty() && fail_msgs.empty()); };
-    std::string TraceFailMsgs();
-  };
+     public:
+      // TestFailMsg();
+      void Init(const std::string& _test_case) {
+        test_case = _test_case;
+        fail_msgs.clear();
+      }
+      void AppendMsg(const std::string& fail_msg, const std::string& func_name, const int line);
+      void AppendMsg(const std::string& fail_msg) { fail_msgs.push_back(fail_msg); }
+      bool is_empty() { return !(test_case.empty() && fail_msgs.empty()); };
+      std::string TraceFailMsgs();
+    };
 
-  static std::vector<unsigned int> dtype_list = {
-    // Type.Void,
-    Type.ComplexDouble, Type.ComplexFloat, Type.Double, Type.Float,  Type.Int64, Type.Uint64,
-    Type.Int32,         Type.Uint32,       Type.Int16,  Type.Uint16, Type.Bool};
+    static std::vector<unsigned int> dtype_list = {
+      // Type.Void,
+      Type.ComplexDouble, Type.ComplexFloat, Type.Double, Type.Float,  Type.Int64, Type.Uint64,
+      Type.Int32,         Type.Uint32,       Type.Int16,  Type.Uint16, Type.Bool};
 
-  static std::vector<int> device_list = {
-    Device.cpu,
-    // Device.cuda,  //currently cuda version still not implement
-  };
+    static std::vector<int> device_list = {
+      Device.cpu,
+      // Device.cuda,  //currently cuda version still not implement
+    };
 
-  // Tensor tools
+    // Tensor tools
 
-  // given the tensor T with shape and dtype has been initialzed, set its data as random uniform.
-  void InitTensorUniform(Tensor& T, unsigned int rand_seed = 0);
-  void InitTensorUniform(std::vector<Tensor>& T, unsigned int rand_seed = 0);
+    // given the tensor T with shape and dtype has been initialzed, set its data as random uniform.
+    void InitTensorUniform(Tensor& T, unsigned int rand_seed = 0);
+    void InitTensorUniform(std::vector<Tensor>& T, unsigned int rand_seed = 0);
 
-  bool AreNearlyEqTensor(const Tensor& T1, const Tensor& T2, const cytnx_double tol = 0);
-  bool AreEqTensor(const Tensor& T1, const Tensor& T2);
+    bool AreNearlyEqTensor(const Tensor& T1, const Tensor& T2, const cytnx_double tol = 0);
+    bool AreEqTensor(const Tensor& T1, const Tensor& T2);
 
-  bool AreNearlyEqUniTensor(const UniTensor& Ut1, const UniTensor& Ut2, const cytnx_double tol = 0);
-  bool AreEqUniTensor(const UniTensor& Ut1, const UniTensor& Ut2);
+    bool AreNearlyEqUniTensor(const UniTensor& Ut1, const UniTensor& Ut2,
+                              const cytnx_double tol = 0);
+    bool AreEqUniTensor(const UniTensor& Ut1, const UniTensor& Ut2);
 
-  bool AreElemSame(const Tensor& T1, const std::vector<cytnx_uint64>& idices1, const Tensor& T2,
-                   const std::vector<cytnx_uint64>& idices2);
+    bool AreElemSame(const Tensor& T1, const std::vector<cytnx_uint64>& idices1, const Tensor& T2,
+                     const std::vector<cytnx_uint64>& idices2);
 
-  void InitUniTensorUniform(UniTensor& UT, unsigned int rand_seed = 0);
-}  // namespace TestTools
+    void InitUniTensorUniform(UniTensor& UT, unsigned int rand_seed = 0);
+  }  // namespace TestTools
+}  // namespace cytnx
 
-#endif
+#endif  // GPU_TEST_TOOLS_H_
