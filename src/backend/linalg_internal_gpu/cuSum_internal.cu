fd1abfd257667a157eef606f59e63b2df3f8fbc3
Ivana <ivana.gyro@gmail.com>
2024-10-06 23:10:33 +0800

    Templatize cuReduce_gpu and use libcu++'s complex
    This commit templatizes the cuReduce_gpu functions and replaces the CUDA
    complex number with the complex number type provided by libcu++. Both
    implementations support alignment settings, ensuring compatibility and
    maintaining performance.
    
    The complex number type from libcu++ is more convenient to use because
    it overloads several operators, allowing for more intuitive arithmetic
    operations.

--- src/backend/linalg_internal_gpu/cuSum_internal.cu
+++ src/backend/linalg_internal_gpu/cuSum_internal.cu
@@ -12,54 +12,53 @@ namespace cytnx {
     void cuSum_internal_cd(boost::intrusive_ptr<Storage_base> &out,
                            const boost::intrusive_ptr<Storage_base> &ten,
                            const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_cd((cytnx_complex128 *)out->Mem, (cytnx_complex128 *)ten->Mem,
-                                      Nelem);
+      utils_internal::cuReduce_gpu((cytnx_complex128 *)out->Mem, (cytnx_complex128 *)ten->Mem,
+                                   Nelem);
     }
     void cuSum_internal_cf(boost::intrusive_ptr<Storage_base> &out,
                            const boost::intrusive_ptr<Storage_base> &ten,
                            const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_cf((cytnx_complex64 *)out->Mem, (cytnx_complex64 *)ten->Mem,
-                                      Nelem);
+      utils_internal::cuReduce_gpu((cytnx_complex64 *)out->Mem, (cytnx_complex64 *)ten->Mem, Nelem);
     }
     void cuSum_internal_d(boost::intrusive_ptr<Storage_base> &out,
                           const boost::intrusive_ptr<Storage_base> &ten,
                           const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_d((cytnx_double *)out->Mem, (cytnx_double *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_double *)out->Mem, (cytnx_double *)ten->Mem, Nelem);
     }
     void cuSum_internal_f(boost::intrusive_ptr<Storage_base> &out,
                           const boost::intrusive_ptr<Storage_base> &ten,
                           const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_f((cytnx_float *)out->Mem, (cytnx_float *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_float *)out->Mem, (cytnx_float *)ten->Mem, Nelem);
     }
     void cuSum_internal_i64(boost::intrusive_ptr<Storage_base> &out,
                             const boost::intrusive_ptr<Storage_base> &ten,
                             const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_i64((cytnx_int64 *)out->Mem, (cytnx_int64 *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_int64 *)out->Mem, (cytnx_int64 *)ten->Mem, Nelem);
     }
     void cuSum_internal_u64(boost::intrusive_ptr<Storage_base> &out,
                             const boost::intrusive_ptr<Storage_base> &ten,
                             const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_u64((cytnx_uint64 *)out->Mem, (cytnx_uint64 *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_uint64 *)out->Mem, (cytnx_uint64 *)ten->Mem, Nelem);
     }
     void cuSum_internal_i32(boost::intrusive_ptr<Storage_base> &out,
                             const boost::intrusive_ptr<Storage_base> &ten,
                             const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_i32((cytnx_int32 *)out->Mem, (cytnx_int32 *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_int32 *)out->Mem, (cytnx_int32 *)ten->Mem, Nelem);
     }
     void cuSum_internal_u32(boost::intrusive_ptr<Storage_base> &out,
                             const boost::intrusive_ptr<Storage_base> &ten,
                             const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_u32((cytnx_uint32 *)out->Mem, (cytnx_uint32 *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_uint32 *)out->Mem, (cytnx_uint32 *)ten->Mem, Nelem);
     }
     void cuSum_internal_i16(boost::intrusive_ptr<Storage_base> &out,
                             const boost::intrusive_ptr<Storage_base> &ten,
                             const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_i16((cytnx_int16 *)out->Mem, (cytnx_int16 *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_int16 *)out->Mem, (cytnx_int16 *)ten->Mem, Nelem);
     }
     void cuSum_internal_u16(boost::intrusive_ptr<Storage_base> &out,
                             const boost::intrusive_ptr<Storage_base> &ten,
                             const cytnx_uint64 &Nelem) {
-      utils_internal::cuReduce_gpu_u16((cytnx_uint16 *)out->Mem, (cytnx_uint16 *)ten->Mem, Nelem);
+      utils_internal::cuReduce_gpu((cytnx_uint16 *)out->Mem, (cytnx_uint16 *)ten->Mem, Nelem);
     }
     void cuSum_internal_b(boost::intrusive_ptr<Storage_base> &out,
                           const boost::intrusive_ptr<Storage_base> &ten,
