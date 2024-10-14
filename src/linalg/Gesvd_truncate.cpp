bb42f5a57c701bc5dda14598b19e4d7bb7e858a6
Manuel Schneider <manuel.schneider@nycu.edu.tw>
2024-10-14 19:12:08 +0800

    -added (Ge)Svd_truncate with minblockdim; a minimal dimension for each block can be given -changed default arguments everywhere to mindim=1 -changed argument types from unsigned int to cytnx_uint

--- src/linalg/Gesvd_truncate.cpp
+++ src/linalg/Gesvd_truncate.cpp
@@ -22,7 +22,7 @@ namespace cytnx {
     typedef Accessor ac;
     std::vector<Tensor> Gesvd_truncate(const Tensor &Tin, const cytnx_uint64 &keepdim,
                                        const double &err, const bool &is_U, const bool &is_vT,
-                                       const unsigned int &return_err, const unsigned int &mindim) {
+                                       const unsigned int &return_err, const cytnx_uint64 &mindim) {
       // check input arguments
       cytnx_error_msg(mindim < 0, "[ERROR][Gesvd_truncate] mindim must be >=1.%s", "\n");
       cytnx_error_msg(keepdim < 1, "[ERROR][Gesvd_truncate] keepdim must be >=1.%s", "\n");
@@ -110,7 +110,7 @@ namespace cytnx {
     void _gesvd_truncate_Dense_UT(std::vector<UniTensor> &outCyT, const cytnx::UniTensor &Tin,
                                   const cytnx_uint64 &keepdim, const double &err, const bool &is_U,
                                   const bool &is_vT, const unsigned int &return_err,
-                                  const unsigned int &mindim) {
+                                  const cytnx_uint64 &mindim) {
       // DenseUniTensor:
       cytnx_uint64 keep_dim = keepdim;
 
@@ -137,7 +137,7 @@ namespace cytnx {
       outCyT.resize(outT.size());
 
       // s
-      // cytnx_error_msg(keepdim>outT[t].shape()[0],"[ERROR][Svd_truncate] keepdim should <=
+      // cytnx_error_msg(keepdim>outT[t].shape()[0],"[ERROR][Gesvd_truncate] keepdim should <=
       // dimension of singular tensor%s","\n");
 
       cytnx::UniTensor &Cy_S = outCyT[t];
@@ -214,7 +214,7 @@ namespace cytnx {
     void _gesvd_truncate_Block_UT(std::vector<UniTensor> &outCyT, const cytnx::UniTensor &Tin,
                                   const cytnx_uint64 &keepdim, const double &err, const bool &is_U,
                                   const bool &is_vT, const unsigned int &return_err,
-                                  const unsigned int &mindim) {
+                                  const cytnx_uint64 &mindim) {
       cytnx_uint64 keep_dim = keepdim;
 
       outCyT = linalg::Gesvd(Tin, is_U, is_vT);
@@ -263,9 +263,9 @@ namespace cytnx {
       for (int b = 0; b < S.Nblocks(); b++) {
         Storage stmp = S.get_block_(b).storage();
         cytnx_int64 kdim = 0;
-        for (int i = stmp.size() - 1; i >= 0; i--) {
-          if (stmp(i) >= Smin) {
-            kdim = i + 1;
+        for (int i = stmp.size(); i > 0; i--) {
+          if (stmp(i - 1) >= Smin) {
+            kdim = i;
             break;
           }
         }
@@ -359,7 +359,7 @@ namespace cytnx {
                                                  const cytnx_uint64 &keepdim, const double &err,
                                                  const bool &is_U, const bool &is_vT,
                                                  const unsigned int &return_err,
-                                                 const unsigned int &mindim) {
+                                                 const cytnx_uint64 &mindim) {
       // using rowrank to split the bond to form a matrix.
       cytnx_error_msg(
         (Tin.rowrank() < 1 || Tin.rank() == 1 || Tin.rowrank() == Tin.rank()),
@@ -378,6 +378,249 @@ namespace cytnx {
         _gesvd_truncate_Block_UT(outCyT, Tin, keepdim, err, is_U, is_vT, return_err, mindim);
       } else {
         cytnx_error_msg(true, "[ERROR] only support gesvd for Dense and Block UniTensor.%s", "\n");
+      }
+      return outCyT;
+
+    }  // Gesvd_truncate
+
+    void _gesvd_truncate_Block_UT(std::vector<UniTensor> &outCyT, const cytnx::UniTensor &Tin,
+                                  const cytnx_uint64 &keepdim,
+                                  const std::vector<cytnx_uint64> minblockdim, const double &err,
+                                  const bool &is_U, const bool &is_vT,
+                                  const unsigned int &return_err, const cytnx_uint64 &mindim) {
+      // currently, Gesvd is used as a standard for the full SVD before truncation
+      cytnx_int64 keep_dim = keepdim;  // these must be signed int, because they can become
+                                       // negative!
+      cytnx_int64 min_dim = (mindim < 1 ? 1 : mindim);
+      std::vector<cytnx_uint64> min_blockdim = minblockdim;
+
+      outCyT = linalg::Gesvd(Tin, is_U, is_vT);
+      if (min_blockdim.size() == 1)  // if only one element given, make it a vector
+        min_blockdim = std::vector<cytnx_uint64>(outCyT[0].Nblocks(), min_blockdim[0]);
+      cytnx_error_msg(
+        min_blockdim.size() != outCyT[0].Nblocks(),
+        "[ERROR][Gesvd_truncate] minblockdim must have the same number of elements as "
+        "blocks in the singular value UniTensor%s",
+        "\n");
+
+      // process truncation:
+      // 1) concate all S vals from all blk but exclude the firt min_blockdim Svals in each block
+      // (since they will be kept anyways later)
+      Tensor Sall;  // S vals excluding the already kept ones
+      Tensor Block;  // current block
+      cytnx_uint64 blockdim;
+      bool anySall = false;  // are there already any values in Sall vals?
+      bool any_min_blockdim = false;  // is any min_blockdim > 0?
+      for (int b = 0; b < outCyT[0].Nblocks(); b++) {
+        if (min_blockdim[b] < 1)  // save whole block to Sall
+          Block = outCyT[0].get_block_(b);
+        else {
+          any_min_blockdim = true;
+          blockdim = outCyT[0].get_block_(b).shape()[0];
+          if (blockdim <= min_blockdim[b]) {
+            // keep whole block
+            keep_dim -= blockdim;
+            min_dim -= blockdim;
+            continue;
+          }
+          // remove first min_blockdim[b] values
+          blockdim = outCyT[0].get_block_(b).shape()[0];
+          Block = outCyT[0].get_block_(b).get({ac::range(min_blockdim[b], blockdim)});
+          keep_dim -= min_blockdim[b];
+          min_dim -= min_blockdim[b];
+        }
+        if (anySall)
+          Sall = algo::Concatenate(Sall, Block);
+        else {
+          Sall = Block;
+          anySall = true;
+        }
+      }
+      if (!anySall) {
+        // no truncation; return_err is tensor with one element, set to 0
+        if (return_err >= 1) {
+          outCyT.push_back(UniTensor(Tensor({1}, Tin.dtype())));
+        }
+      } else {
+        Scalar Smin;
+        if (keep_dim > 0) {
+          if (!any_min_blockdim) {
+            // make sure that at least one singular value is kept
+            min_dim = (min_dim < 1 ? 1 : min_dim);
+          } else {
+            min_dim = (min_dim < 1 ? 0 : min_dim);
+          }
+          Sall = algo::Sort(Sall);  // all singular values, starting from the smallest
+          // 2) get the minimum S value based on the args input.
+          cytnx_uint64 smidx;
+          cytnx_uint64 Sshape = Sall.shape()[0];
+          if (keep_dim < Sshape) {
+            smidx = Sshape - (cytnx_uint64)keep_dim;
+            Smin = Sall.storage()(smidx);
+          } else {
+            keep_dim = Sshape;
+            smidx = 0;
+            Smin = Sall.storage()(0);
+          }
+          while ((Smin < err) and (keep_dim > min_dim)) {
+            // at least one singular value is always kept!
+            keep_dim--;
+            if (keep_dim == 0) break;  // this is needed, keep_dim can be 0
+            smidx++;
+            Smin = Sall.storage()(smidx);
+          }
+          // handle return_err!
+          if (return_err == 1) {
+            outCyT.push_back(UniTensor(Tensor({1}, Smin.dtype())));
+            outCyT.back().get_block_().storage().at(0) = Smin;
+          } else if (return_err) {
+            outCyT.push_back(UniTensor(Sall.get({ac::tilend(smidx)})));
+          }
+        } else {
+          if (return_err >= 1) {
+            outCyT.push_back(UniTensor(Tensor({1}, Tin.dtype())));
+          }
+        }
+
+        // traversal each block and truncate!
+        UniTensor &S = outCyT[0];
+        std::vector<cytnx_uint64> new_dims;  // keep_dims for each block!
+        std::vector<cytnx_int64> keep_dims;
+        keep_dims.reserve(S.Nblocks());
+        std::vector<cytnx_int64> new_qid;
+        new_qid.reserve(S.Nblocks());
+
+        std::vector<std::vector<cytnx_uint64>>
+          new_itoi;  // assume S block is in same order as qnum:
+        std::vector<cytnx_uint64> to_be_remove;
+
+        cytnx_uint64 tot_dim = 0;
+        cytnx_uint64 cnt = 0;
+        for (int b = 0; b < S.Nblocks(); b++) {
+          Storage stmp = S.get_block_(b).storage();
+          cytnx_int64 kdim = min_blockdim[b];
+          if (keep_dim > 0) {
+            // search for first value >= Smin
+            for (int i = stmp.size(); i > min_blockdim[b]; i--) {
+              // Careful here: if (int i = stmp.size() -1; i >= min_blockdim[b]; i--) is used
+              // instead, the compiler might make i an unsigned integer; if then min_blockdim[b] ==
+              // 0, the condition i > min_blockdim[b] is always fulfilled and the loop never stops!
+              if (stmp(i - 1) >= Smin) {
+                kdim = i;
+                break;
+              }
+            }
+          }
+          keep_dims.push_back(kdim);
+          if (kdim == 0) {
+            to_be_remove.push_back(b);
+            new_qid.push_back(-1);
+          } else {
+            new_qid.push_back(new_dims.size());
+            new_itoi.push_back({new_dims.size(), new_dims.size()});
+            new_dims.push_back(kdim);
+            tot_dim += kdim;
+            if (kdim != S.get_blocks_()[b].shape()[0])
+              S.get_blocks_()[b] = S.get_blocks_()[b].get({ac::range(0, kdim)});
+          }
+        }
+
+        // remove:
+        // vec_erase_(S.get_itoi(),to_be_remove);
+        S.get_itoi() = new_itoi;
+        vec_erase_(S.get_blocks_(), to_be_remove);
+        vec_erase_(S.bonds()[0].qnums(), to_be_remove);
+        S.bonds()[0]._impl->_degs = new_dims;
+        S.bonds()[0]._impl->_dim = tot_dim;
+        S.bonds()[1] = S.bonds()[0].redirect();
+
+        int t = 1;
+        if (is_U) {
+          UniTensor &U = outCyT[t];
+          to_be_remove.clear();
+          U.bonds().back() = S.bonds()[1].clone();
+          std::vector<Accessor> acs(U.rank());
+          for (int i = 0; i < U.rowrank(); i++) acs[i] = ac::all();
+
+          for (int b = 0; b < U.Nblocks(); b++) {
+            if (keep_dims[U.get_qindices(b).back()] == 0)
+              to_be_remove.push_back(b);
+            else {
+              /// process blocks:
+              if (keep_dims[U.get_qindices(b).back()] != U.get_blocks_()[b].shape().back()) {
+                acs.back() = ac::range(0, keep_dims[U.get_qindices(b).back()]);
+                U.get_blocks_()[b] = U.get_blocks_()[b].get(acs);
+              }
+
+              // change to new qindices:
+              U.get_qindices(b).back() = new_qid[U.get_qindices(b).back()];
+            }
+          }
+          vec_erase_(U.get_itoi(), to_be_remove);
+          vec_erase_(U.get_blocks_(), to_be_remove);
+
+          t++;
+        }
+
+        if (is_vT) {
+          UniTensor &vT = outCyT[t];
+          to_be_remove.clear();
+          vT.bonds().front() = S.bonds()[0].clone();
+          std::vector<Accessor> acs(vT.rank());
+          for (int i = 1; i < vT.rank(); i++) acs[i] = ac::all();
+
+          for (int b = 0; b < vT.Nblocks(); b++) {
+            if (keep_dims[vT.get_qindices(b)[0]] == 0)
+              to_be_remove.push_back(b);
+            else {
+              /// process blocks:
+              if (keep_dims[vT.get_qindices(b)[0]] != vT.get_blocks_()[b].shape()[0]) {
+                acs[0] = ac::range(0, keep_dims[vT.get_qindices(b)[0]]);
+                vT.get_blocks_()[b] = vT.get_blocks_()[b].get(acs);
+              }
+              // change to new qindices:
+              vT.get_qindices(b)[0] = new_qid[vT.get_qindices(b)[0]];
+            }
+          }
+          vec_erase_(vT.get_itoi(), to_be_remove);
+          vec_erase_(vT.get_blocks_(), to_be_remove);
+          t++;
+        }
+      }
+    }  // _gesvd_truncate_Block_UT
+
+    std::vector<cytnx::UniTensor> Gesvd_truncate(const cytnx::UniTensor &Tin,
+                                                 const cytnx_uint64 &keepdim,
+                                                 const std::vector<cytnx_uint64> minblockdim,
+                                                 const double &err, const bool &is_U,
+                                                 const bool &is_vT, const unsigned int &return_err,
+                                                 const cytnx_uint64 &mindim) {
+      // using rowrank to split the bond to form a matrix.
+      cytnx_error_msg(
+        (Tin.rowrank() < 1 || Tin.rank() == 1 || Tin.rowrank() == Tin.rank()),
+        "[ERROR][Gesvd_truncate] UniTensor should have rank>1 and rank>rowrank>0 for Svd%s", "\n");
+
+      // check input arguments
+      // cytnx_error_msg(mindim < 0, "[ERROR][Gesvd_truncate] mindim must be >=1%s", "\n");
+      cytnx_error_msg(keepdim < 1, "[ERROR][Gesvd_truncate] keepdim must be >=1%s", "\n");
+      // cytnx_error_msg(return_err < 0, "[ERROR][Gesvd_truncate] return_err cannot be negative%s",
+      //                 "\n");
+
+      std::vector<UniTensor> outCyT;
+      if (Tin.uten_type() == UTenType.Dense) {
+        cytnx_error_msg(
+          minblockdim.size() != 1,
+          "[ERROR][Gesvd_truncate] minblockdim must have one element for dense UniTensor%s", "\n");
+        _gesvd_truncate_Dense_UT(outCyT, Tin, keepdim, err, is_U, is_vT, return_err,
+                                 max(mindim, minblockdim[0]));
+
+      } else if (Tin.uten_type() == UTenType.Block) {
+        _gesvd_truncate_Block_UT(outCyT, Tin, keepdim, minblockdim, err, is_U, is_vT, return_err,
+                                 mindim);
+
+      } else {
+        cytnx_error_msg(
+          true, "[ERROR][Gesvd_truncate] only Dense/Block UniTensors are supported.%s", "\n");
       }
       return outCyT;
 
