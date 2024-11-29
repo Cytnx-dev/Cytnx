7fbd8db8713772fb8b14e4c610b962087018d652
Ivana <ivana.gyro@gmail.com>
2024-11-30 00:44:02 +0800

    Get number of CPU cores with std::thread
    Get the correct number of CPU cores when OpenMP is not used.

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,4 +1,7 @@
 #include "Device.hpp"
+
+#include <thread>
+
 #include "cytnx_error.hpp"
 #ifdef UNI_OMP
   #include <omp.h>
@@ -7,7 +10,7 @@
 using namespace std;
 namespace cytnx {
 
-  Device_class::Device_class() : Ngpus(0), Ncpus(1) {
+  Device_class::Device_class() : Ngpus(0), Ncpus(std::thread::hardware_concurrency()) {
     // cout << "init_device class!" << endl;
 #ifdef UNI_GPU
 
@@ -38,17 +41,7 @@ namespace cytnx {
         }
       }
     }
-
-#endif
-
-#ifdef UNI_OMP
-  #pragma omp parallel
-    {
-      if (omp_get_thread_num() == 0) {
-        Ncpus = omp_get_num_threads();
-      }
-    }
-#endif
+#endif  // UNI_GPU
   };
 
   Device_class::~Device_class(){
