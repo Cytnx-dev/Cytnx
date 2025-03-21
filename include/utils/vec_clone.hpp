d3d0a8db962f39fbf40d86b2adc4487bfb761d36
Ivana <ivana.gyro@gmail.com>
2025-03-22 00:16:32 +0800

    Minimize definition of vec_clone function
    Only keep the vec_clone definition which are in use.
    
    Recommend to use the constructor of std::vector or std::transform in
    the case that needs the vec_clone in the future.

--- include/utils/vec_clone.hpp
+++ include/utils/vec_clone.hpp
@@ -9,15 +9,7 @@ namespace cytnx {
   std::vector<T> vec_clone(const std::vector<T>& in_vec);
 
   template <class T>
-  std::vector<T> vec_clone(const std::vector<T>& in_vec, const cytnx_uint64& Nelem);
-
-  template <class T>
   std::vector<T> vec_clone(const std::vector<T>& in_vec, const std::vector<cytnx_uint64>& locators);
-
-  template <class T>
-  std::vector<T> vec_clone(const std::vector<T>& in_vec, const cytnx_uint64& start,
-                           const cytnx_uint64& end);
-
 }  // namespace cytnx
 
 #endif  // CYTNX_UTILS_VEC_CLONE_H_
--- src/utils/vec_clone.cpp
+++ src/utils/vec_clone.cpp
@@ -24,151 +24,6 @@ namespace cytnx {
   }
   //=======================================================
 
-  template <class T>
-  std::vector<T> vec_clone(const std::vector<T> &in_vec, const cytnx_uint64 &Nelem) {
-    cytnx_error_msg(Nelem > in_vec.size(),
-                    "[ERROR] Nelem cannot exceed the no. of elements in the in_vec%s", "\n");
-    std::vector<T> out(Nelem);
-    for (cytnx_uint64 i = 0; i < Nelem; i++) {
-      out[i] = in_vec[i].clone();
-    }
-    return out;
-  }
-  //========================================================
-
-  template <class T>
-  std::vector<T> vec_clone(const std::vector<T> &in_vec,
-                           const std::vector<cytnx_uint64> &locators) {
-    std::vector<T> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]].clone();
-    }
-    return out;
-  }
-
-  template <>
-  std::vector<cytnx_complex128> vec_clone(const std::vector<cytnx_complex128> &in_vec,
-                                          const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_complex128> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_complex64> vec_clone(const std::vector<cytnx_complex64> &in_vec,
-                                         const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_complex64> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<double> vec_clone(const std::vector<double> &in_vec,
-                                const std::vector<cytnx_uint64> &locators) {
-    std::vector<double> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<float> vec_clone(const std::vector<float> &in_vec,
-                               const std::vector<cytnx_uint64> &locators) {
-    std::vector<float> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_int64> vec_clone(const std::vector<cytnx_int64> &in_vec,
-                                     const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_int64> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_uint64> vec_clone(const std::vector<cytnx_uint64> &in_vec,
-                                      const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_uint64> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_int32> vec_clone(const std::vector<cytnx_int32> &in_vec,
-                                     const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_int32> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_uint32> vec_clone(const std::vector<cytnx_uint32> &in_vec,
-                                      const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_uint32> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_int16> vec_clone(const std::vector<cytnx_int16> &in_vec,
-                                     const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_int16> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_uint16> vec_clone(const std::vector<cytnx_uint16> &in_vec,
-                                      const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_uint16> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
-  template <>
-  std::vector<cytnx_bool> vec_clone(const std::vector<cytnx_bool> &in_vec,
-                                    const std::vector<cytnx_uint64> &locators) {
-    std::vector<cytnx_bool> out(locators.size());
-    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
-      cytnx_error_msg(locators[i] >= in_vec.size(),
-                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
-      out[i] = in_vec[locators[i]];
-    }
-    return out;
-  }
   template <>
   std::vector<std::string> vec_clone(const std::vector<std::string> &in_vec,
                                      const std::vector<cytnx_uint64> &locators) {
@@ -180,23 +35,18 @@ namespace cytnx {
     }
     return out;
   }
-  //=============================================================================
 
-  template <class T>
-  std::vector<T> vec_clone(const std::vector<T> &in_vec, const cytnx_uint64 &start,
-                           const cytnx_uint64 &end) {
-    cytnx_error_msg(start >= end, "[ERROR][vec_clone] start must <= end.%s", "\n");
-    cytnx_error_msg((end - start) > in_vec.size(),
-                    "[ERROR][vec_clone] indices out of range for clone.%s", "\n");
-
-    std::vector<T> out(end - start);
-    for (cytnx_uint64 i = start; i < end; i++) {
-      out[i - start] = in_vec[i].clone();
+  template <>
+  std::vector<cytnx_uint64> vec_clone(const std::vector<cytnx_uint64> &in_vec,
+                                      const std::vector<cytnx_uint64> &locators) {
+    std::vector<cytnx_uint64> out(locators.size());
+    for (cytnx_uint64 i = 0; i < locators.size(); i++) {
+      cytnx_error_msg(locators[i] >= in_vec.size(),
+                      "[ERROR] the index [%d] in locators exceed the bbound.\n", locators[i]);
+      out[i] = in_vec[locators[i]];
     }
     return out;
   }
-
-  //================================
 
 #ifdef BACKEND_TORCH
 #else
@@ -205,39 +55,9 @@ namespace cytnx {
   template std::vector<Storage> vec_clone<Storage>(const std::vector<Storage> &);
   template std::vector<UniTensor> vec_clone<UniTensor>(const std::vector<UniTensor> &);
 
-  template std::vector<Tensor> vec_clone<Tensor>(const std::vector<Tensor> &, const cytnx_uint64 &);
-  template std::vector<Storage> vec_clone<Storage>(const std::vector<Storage> &,
-                                                   const cytnx_uint64 &);
-  template std::vector<UniTensor> vec_clone<UniTensor>(const std::vector<UniTensor> &,
-                                                       const cytnx_uint64 &);
-
-  template std::vector<Tensor> vec_clone<Tensor>(const std::vector<Tensor> &,
-                                                 const std::vector<cytnx_uint64> &);
-  template std::vector<Storage> vec_clone<Storage>(const std::vector<Storage> &,
-                                                   const std::vector<cytnx_uint64> &);
-  template std::vector<UniTensor> vec_clone<UniTensor>(const std::vector<UniTensor> &,
-                                                       const std::vector<cytnx_uint64> &);
-
-  template std::vector<Tensor> vec_clone<Tensor>(const std::vector<Tensor> &, const cytnx_uint64 &,
-                                                 const cytnx_uint64 &);
-  template std::vector<Storage> vec_clone<Storage>(const std::vector<Storage> &,
-                                                   const cytnx_uint64 &, const cytnx_uint64 &);
-  template std::vector<UniTensor> vec_clone<UniTensor>(const std::vector<UniTensor> &,
-                                                       const cytnx_uint64 &, const cytnx_uint64 &);
 #endif
 
   template std::vector<Bond> vec_clone<Bond>(const std::vector<Bond> &);
   template std::vector<Symmetry> vec_clone<Symmetry>(const std::vector<Symmetry> &);
-  template std::vector<Bond> vec_clone<Bond>(const std::vector<Bond> &, const cytnx_uint64 &);
-  template std::vector<Symmetry> vec_clone<Symmetry>(const std::vector<Symmetry> &,
-                                                     const cytnx_uint64 &);
-  template std::vector<Bond> vec_clone<Bond>(const std::vector<Bond> &,
-                                             const std::vector<cytnx_uint64> &);
-  template std::vector<Symmetry> vec_clone<Symmetry>(const std::vector<Symmetry> &,
-                                                     const std::vector<cytnx_uint64> &);
-  template std::vector<Bond> vec_clone<Bond>(const std::vector<Bond> &, const cytnx_uint64 &,
-                                             const cytnx_uint64 &);
-  template std::vector<Symmetry> vec_clone<Symmetry>(const std::vector<Symmetry> &,
-                                                     const cytnx_uint64 &, const cytnx_uint64 &);
 
 }  // namespace cytnx
