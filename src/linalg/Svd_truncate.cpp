72f8c934401c7db190d4d7850d36c55d09fc7e31
Manuel Schneider <manuel.schneider@nycu.edu.tw>
2025-05-02 13:16:49 +0800

    some debugging lines

--- src/linalg/Svd_truncate.cpp
+++ src/linalg/Svd_truncate.cpp
@@ -410,10 +410,18 @@ namespace cytnx {
       vec_erase_(signs, to_be_remove);
       cout << "Signflips after  truncation: " << S.signflip() << endl;
       cout << "signs after  truncation: " << signs << endl;
-      cytnx_error_msg(true, "[Svdtruncate][ERROR] Svdtruncate still under development%s", "\n");
       S.bonds()[0]._impl->_degs = new_dims;
       S.bonds()[0]._impl->_dim = tot_dim;
       S.bonds()[1] = S.bonds()[0].redirect();
+      // to access _signflip in the tensor, a typecast is needed
+      cytnx_error_msg(true, "[Svdtruncate][ERROR] Svdtruncate still under development%s", "\n");
+      // boost:intrusive_ptr<cytnx::BlockFermionicUniTensor> Sfermionic = boost:static_pointer_cast<cytnx::BlockFermionicUniTensor>(S._impl);
+      // BlockFermionicUniTensor &Sfermionic = boost::dynamic_pointer_cast<cytnx::BlockFermionicUniTensor&>(S._impl);
+      // Sfermionic.print_diagram();
+      // Sfermionic._signflip = signs;
+      // S.print_diagram();
+      // cytnx_error_msg(true, "[Svdtruncate][ERROR] Svdtruncate still under development%s", "\n");
+
 
       int t = 1;
       if (is_UvT) {
