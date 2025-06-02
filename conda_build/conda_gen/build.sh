2b61646f8e3395e623e494b718c7f7adec18d639
Ivana <ivana.gyro@gmail.com>
2025-06-02 16:53:15 +0800

    Adds GitHub workflow for caching the main branch
    Sets up a new GitHub Actions workflow to cache the build result of the
    main branch. The build result cached by this GitHub action can be loaded
    by the action triggered by the PR that requests for merging into the
    main branch.

--- conda_build/conda_gen/build.sh
+++ conda_build/conda_gen/build.sh
@@ -4,4 +4,11 @@
 #make install -j4
 
 # $PYTHON setup.py install     # Python command to install the script.
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
+
+# By default, setuptools creates separate working directories for each Python
+# version. This prevents ccache from sharing cached objects between versions.
+# Setting the working directory to "build" ensures ccache can reuse the cache
+# across different Python versions.
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation\
+  --config-settings=--build-option=build_ext\
+  --config-settings=--build-option=--build-temp=build
