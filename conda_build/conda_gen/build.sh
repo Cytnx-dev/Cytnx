c4311a7938349b6c97417772d3dafe47c88d03fa
Ivana <ivana.gyro@gmail.com>
2025-06-07 16:17:43 +0800

    Print execution time in the script for conda build
    Execution time helps developers to know if ccache hits the cache
    expectedly.
    
    This change also removes unsued commands and change the
    `--no-build-isolation` flag for `pip install` to `--ignore-installed`.
    The correction of the flag is to ensure every build is isolated.

--- conda_build/conda_gen/build.sh
+++ conda_build/conda_gen/build.sh
@@ -1,14 +1,12 @@
-#mkdir build
-#cd build
-#cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DUSE_MKL=1
-#make install -j4
-
-# $PYTHON setup.py install     # Python command to install the script.
+start_time=$(date +%s)
 
 # By default, setuptools creates separate working directories for each Python
 # version. This prevents ccache from sharing cached objects between versions.
 # Setting the working directory to "build" ensures ccache can reuse the cache
 # across different Python versions.
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation\
+$PYTHON -m pip install . -vv --no-deps --ignore-installed\
   --config-settings=--build-option=build_ext\
   --config-settings=--build-option=--build-temp=build
+
+end_time=$(date +%s)
+echo "Execution time: $((end_time - start_time)) seconds"
