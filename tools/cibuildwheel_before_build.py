9b26304e0a8f80477b7c1b5387308ea6ac880d16
Ivana <ivana.gyro@gmail.com>
2025-11-22 03:18:07 +0800

    set up cibuildwheel

---
+++ tools/cibuildwheel_before_build.py
@@ -0,0 +1,22 @@
+import os
+import pathlib
+import sys
+
+import packaging.tags
+tag = f'cp{sys.version_info.major}{sys.version_info.minor}'
+
+ccache_config_path = os.getenv('CCACHE_CONFIGPATH')
+if not ccache_config_path:
+    raise RuntimeError('The CCACHE_CONFIGPATH environment variable must be set.')
+
+print("ccache_config_path:", ccache_config_path)
+print("ccache_config_path:", pathlib.Path(ccache_config_path).absolute())
+
+
+wheel_tag = str(next(packaging.tags.sys_tags()))
+print("wheel_tag:", wheel_tag)
+
+with open(ccache_config_path, 'w') as f:
+    f.writelines([
+        f'base_dir = /project/build/{wheel_tag}'
+    ])
