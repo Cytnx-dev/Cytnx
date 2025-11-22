e7a1dfb57d2cc249968242ad34e39eadddb2a75d
hunghaoti <hunghaoti852@gmail.com>
2025-11-22 12:16:31 +0800

    Fix link.

--- docs/api_docs/home_source/conf.py
+++ docs/api_docs/home_source/conf.py
@@ -100,7 +100,7 @@ html_theme_options = {
     # True indicates an external link.
     # False indicates path of pages in the document.
     'navbar_links' : [
-         ("User Guide", "../../../build/html/index.html", True),
+         ("User Guide", "../../html/index.html", True),
          ("Github", "https://github.com/Cytnx-dev/Cytnx", True)
     ],
 
