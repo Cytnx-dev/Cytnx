e87c3a211c06129c325887994abb2b32c3b505e5
Manuel Schneider <manuel.schneider@nycu.edu.tw>
2025-11-29 07:54:42 +0800

    clang

--- tests/BlockFermionicUniTensor_test.h
+++ tests/BlockFermionicUniTensor_test.h
@@ -22,8 +22,9 @@ class BlockFermionicUniTensorTest : public ::testing::Test {
   UniTensor BFUT1 = UniTensor({B1, B2, B12}, {"a", "b", "c"}).set_name("BFUT1");
   UniTensor BFUT2;
   UniTensor BFUT3 = UniTensor({B1, B2, B12, B3, B4}, {"a", "b", "c", "d", "e"}).set_name("BFUT3");
-  UniTensor BFUT3INV = UniTensor({B1, B2, B12, B3, B4}, {"a", "b", "c", "d", "e"})
-                          .set_name("BFUT3INV");;
+  UniTensor BFUT3INV =
+    UniTensor({B1, B2, B12, B3, B4}, {"a", "b", "c", "d", "e"}).set_name("BFUT3INV");
+  ;
   UniTensor BFUT3PERM = UniTensor({B3, B2, B4, B12, B1}, {"d", "b", "e", "c", "a"})
                           .set_name("BFUT3PERM");  // permutation {3, 1, 4, 2, 0}
   UniTensor BFUT4 = UniTensor({B1g, B2g, B3g}, {"a", "b", "c"}).set_name("BFUT4");
