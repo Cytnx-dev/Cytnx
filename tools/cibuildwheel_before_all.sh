04934a91947e1c32c5acf36113631c6c8388edaa
b95702041 <b95702041@gmail.com>
2026-01-31 07:08:54 +0000

    Fix: add cblas.h symlink for Linux builds
    - Create symlink from /usr/include/openblas/cblas.h to /usr/include/cblas.h
    - Fixes compilation error: 'cblas.h: No such file or directory'
    - Similar to existing lapacke.h, lapack.h symlinks

--- tools/cibuildwheel_before_all.sh
+++ tools/cibuildwheel_before_all.sh
@@ -11,7 +11,7 @@ ls /usr/include
 ln -s /usr/include/openblas/lapacke.h /usr/include/lapacke.h
 ln -s /usr/include/openblas/lapack.h /usr/include/lapack.h
 ln -s /usr/include/openblas/lapacke_mangling.h /usr/include/lapacke_mangling.h
-
+ln -s /usr/include/openblas/cblas.h /usr/include/cblas.h
 ls /usr/include/openblas
 #  boost1.78-devel
 # ls /usr/include/boost | head
