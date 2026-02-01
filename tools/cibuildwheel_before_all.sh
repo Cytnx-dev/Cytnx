44ad9bc86b29da4ea1582aa64d27553e99470970
b95702041 <b95702041@gmail.com>
2026-02-01 07:05:21 +0000

    Fix: restore all Linux build fixes for manylinux_2_28
    - Install gcc-c++, glibc-devel, kernel-headers
    - Add cblas.h and openblas_config.h symlinks
    - Use ln -sf to force overwrite
    - Add verification steps

--- tools/cibuildwheel_before_all.sh
+++ tools/cibuildwheel_before_all.sh
@@ -1,18 +1,25 @@
 set -xe
 
+# Install core development tools first
+yum install -y gcc-c++ glibc-devel kernel-headers
+
 yum search lapack
 yum --showduplicates list "lapack*"
-# yum install -y arpack-devel boost-devel ccache lapack-devel libomp-devel openblas-devel
 yum install -y arpack-devel boost-devel ccache libomp-devel openblas-devel
+
 # On Red Hat/CentOS systems (which manylinux images are based on), the lapack-devel
 # package typically provides the Fortran bindings and miss the C-interface header, so
-# use lapacke.h shipped with openblas instead.
+# use headers shipped with openblas instead.
 ls /usr/include
-ln -s /usr/include/openblas/lapacke.h /usr/include/lapacke.h
-ln -s /usr/include/openblas/lapack.h /usr/include/lapack.h
-ln -s /usr/include/openblas/lapacke_mangling.h /usr/include/lapacke_mangling.h
-
 ls /usr/include/openblas
-#  boost1.78-devel
-# ls /usr/include/boost | head
-# ls /usr/lib64/libboost_*.so
+
+# Create symlinks for all required OpenBLAS headers
+ln -sf /usr/include/openblas/lapacke.h /usr/include/lapacke.h
+ln -sf /usr/include/openblas/lapack.h /usr/include/lapack.h
+ln -sf /usr/include/openblas/lapacke_mangling.h /usr/include/lapacke_mangling.h
+ln -sf /usr/include/openblas/cblas.h /usr/include/cblas.h
+ln -sf /usr/include/openblas/openblas_config.h /usr/include/openblas_config.h
+
+# Verify symlinks were created
+ls -la /usr/include/cblas.h
+ls -la /usr/include/openblas_config.h
