b029f41e77566910655b6650f28486ef82557df4
b95702041 <b95702041@gmail.com>
2026-02-01 05:39:34 +0000

    fix: restore before_all.sh to working version

--- tools/cibuildwheel_before_all.sh
+++ tools/cibuildwheel_before_all.sh
@@ -1,12 +1,9 @@
 set -xe
 
-# Install core development tools first
-microdnf install -y gcc-c++ glibc-devel kernel-headers
-
-microdnf search lapack
-microdnf --showduplicates list "lapack*"
-# microdnf install -y arpack-devel boost-devel ccache lapack-devel libomp-devel openblas-devel
-microdnf install -y arpack-devel boost-devel ccache libomp-devel openblas-devel
+yum search lapack
+yum --showduplicates list "lapack*"
+# yum install -y arpack-devel boost-devel ccache lapack-devel libomp-devel openblas-devel
+yum install -y arpack-devel boost-devel ccache libomp-devel openblas-devel
 # On Red Hat/CentOS systems (which manylinux images are based on), the lapack-devel
 # package typically provides the Fortran bindings and miss the C-interface header, so
 # use lapacke.h shipped with openblas instead.
@@ -14,8 +11,7 @@ ls /usr/include
 ln -s /usr/include/openblas/lapacke.h /usr/include/lapacke.h
 ln -s /usr/include/openblas/lapack.h /usr/include/lapack.h
 ln -s /usr/include/openblas/lapacke_mangling.h /usr/include/lapacke_mangling.h
-ln -s /usr/include/openblas/cblas.h /usr/include/cblas.h
-ln -s /usr/include/openblas/openblas_config.h /usr/include/openblas_config.h
+
 ls /usr/include/openblas
 #  boost1.78-devel
 # ls /usr/include/boost | head
