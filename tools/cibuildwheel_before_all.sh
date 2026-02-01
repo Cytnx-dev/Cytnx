01a7f3c9d05fcba0a4951e54aa92c7bd0ba8e28d
b95702041 <b95702041@gmail.com>
2026-02-01 16:34:29 +0000

    fix: install boost and dependencies via dnf/yum

--- tools/cibuildwheel_before_all.sh
+++ tools/cibuildwheel_before_all.sh
@@ -1,9 +1,20 @@
 set -xe
+
 # Install ccache from GitHub releases
 CCACHE_VERSION=4.9.1
 curl -L https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/ccache-${CCACHE_VERSION}-linux-x86_64.tar.xz | tar -xJ
 cp ccache-${CCACHE_VERSION}-linux-x86_64/ccache /usr/local/bin/
 ccache --version
+
+# Install required packages
+# manylinux_2_28 is AlmaLinux 8 based, try dnf first, then yum
+if command -v dnf &> /dev/null; then
+    dnf install -y boost-devel openblas-devel arpack-devel
+elif command -v yum &> /dev/null; then
+    yum install -y boost-devel openblas-devel arpack-devel
+else
+    echo "WARNING: No package manager found, assuming packages are pre-installed"
+fi
 
 # Create symlinks for OpenBLAS headers if available
 if [ -d /usr/include/openblas ]; then
