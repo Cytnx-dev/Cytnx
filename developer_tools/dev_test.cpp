3526b8a3b9bf523685adebf690d4e03e40e99fd6
Manuel Schneider <manuel.schneider@nycu.edu.tw>
2026-06-30 01:19:45 +0800

    developer_tools: removed using namespace std;

--- developer_tools/dev_test.cpp
+++ developer_tools/dev_test.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "cytnx.hpp"
 
-using namespace std;
-
 int main(int argc, char* argv[]) {
   auto T = cytnx::random::uniform({3, 4}, -1, 1);
-  cout << T << endl;
+  std::cout << T << std::endl;
   auto T2 = cytnx::random::uniform({3, 4}, -1, 1);
-  cout << T2 << endl;
+  std::cout << T2 << std::endl;
   return 0;
 }
